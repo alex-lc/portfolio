6e976f091c6bbb4a33963ae19f5999db08b2a460
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-09-30 14:49:57 -0500

    Removed Raheem from projects for now. Resume is no longer a new page and just downloads PDF version of resume for more information

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,28 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
 
-import { NavContainer, Nav } from '../styles';
+import { NavContainer, Nav } from "../styles";
+
+// assets
+import Resume from "../assets/files/alexander-cooter-resume-2020.pdf";
 
 export default function Header() {
-    return (
-        <NavContainer>
-            <Nav>
-                <NavLink exact activeClassName="active" to="/">Home</NavLink>
-                <NavLink activeClassName="active" to="/skills">Skills</NavLink>
-                <NavLink activeClassName="active" to="/projects">Projects</NavLink>
-                <NavLink activeClassName="active" to="/resume">Resume</NavLink>
-            </Nav>
-        </NavContainer>
-    )
+  return (
+    <NavContainer>
+      <Nav>
+        <NavLink exact activeClassName="active" to="/">
+          Home
+        </NavLink>
+        <NavLink activeClassName="active" to="/skills">
+          Skills
+        </NavLink>
+        <NavLink activeClassName="active" to="/projects">
+          Projects
+        </NavLink>
+        <Link to={Resume} target="_blank" download>
+          Resume
+        </Link>
+      </Nav>
+    </NavContainer>
+  );
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,37 +1,35 @@
-import React from 'react';
-import { useSpring } from 'react-spring';
+import React from "react";
+import { useSpring } from "react-spring";
 
 /* styles */
-import { Container, InnerContainer, H2, LargeP } from '../../styles';
+import { Container, InnerContainer, H2, LargeP } from "../../styles";
 
 /* components */
-import Header from '../Header';
-import SafeFurDogs from './SafeFurDogs';
-import Raheem from './Raheem';
-import WaterMyPlants from './WaterMyPlants';
+import Header from "../Header";
+import SafeFurDogs from "./SafeFurDogs";
+import WaterMyPlants from "./WaterMyPlants";
 
 export default function Projects() {
+  /* react-spring */
+  const props = useSpring({
+    opacity: 1,
+    marginTop: 0,
+    from: { opacity: 0, marginTop: -500 },
+  });
 
-    /* react-spring */
-    const props = useSpring({
-        opacity: 1, marginTop: 0, from: { opacity: 0, marginTop: -500 }
-    });
+  return (
+    <>
+      <Container style={props}>
+        <InnerContainer>
+          <Header />
 
-    return (
-        <>
-            <Container style={props}>
-                <InnerContainer>
-                    <Header />
+          <H2>Projects</H2>
+          <LargeP>A list of recently completed projects.</LargeP>
+        </InnerContainer>
+      </Container>
 
-                    <H2>Projects</H2>
-                    <LargeP>A list of recently completed projects.</LargeP>
-                </InnerContainer>
-
-            </Container>
-
-            <SafeFurDogs />
-            <Raheem />
-            <WaterMyPlants />
-        </>
-    )
+      <SafeFurDogs />
+      <WaterMyPlants />
+    </>
+  );
 }
