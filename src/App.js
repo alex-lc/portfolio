98617b8b5875bb12a1914b2afda2ffeda2725878
Alex Cooter <56034009+alex-lc@users.noreply.github.com>
2020-09-30 15:18:54 -0500

    Removed Resume components as they are not needed anymore.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { Route } from "react-router-dom";
 
 /* components */
-import Landing from './components/Landing';
-import Skills from './components/Skills';
-import Projects from './components/projects/Projects';
-import Resume from './components/Resume';
+import Landing from "./components/Landing";
+import Skills from "./components/Skills";
+import Projects from "./components/projects/Projects";
 
 function App() {
-
   return (
     <>
       <Route exact path="/" component={Landing} />
       <Route path="/skills" component={Skills} />
       <Route path="/projects" component={Projects} />
-      <Route path="/resume" component={Resume} />
     </>
   );
 }
